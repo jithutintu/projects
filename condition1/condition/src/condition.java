90111fa758329b7543a1b00ef97db2003e259dd3
jithutintu <jithu108@gmail.com>
2016-08-01 01:02:26 -0700

    controlstmt

---
+++ condition1/condition/src/condition.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+class condition
+{
+public static void main(String args[])
+{
+int i;
+Scanner obj=new Scanner(System.in);
+System.out.println("enter the n valuee.....");
+i=obj.nextInt();
+for(i=0;i<=5;i++)
+{
+System.out.println("The values are"+i);
+}
+}
+}
